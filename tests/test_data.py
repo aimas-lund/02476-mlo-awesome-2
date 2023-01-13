6f8482c7c1d7f1cf8341d1d461f51b3e077a63e2
Aimas Lund <33122600+aimas-lund@users.noreply.github.com>
2023-01-13 12:40:12 +0100

    Formatted files to be PEP8 compliant (#16)

--- tests/test_data.py
+++ tests/test_data.py
@@ -12,6 +12,9 @@ def test_cifar10_data_dimensions():
     train_set = CIFAR10Dataset(train=True)
     test_set = CIFAR10Dataset(train=False)
 
-    assert train_set.data.size() == torch.empty(50000, 3, 32, 32).size(), "Expected a training set of 50000 instances with 32x32x3 dimensions."
-    assert test_set.data.size() == torch.empty(10000, 3, 32, 32).size(), "Expected a training set of 10000 instances with 32x32x3 dimensions."
-
+    assert (
+        train_set.data.size() == torch.empty(50000, 3, 32, 32).size()
+    ), "Expected a training set of 50000 instances with 32x32x3 dimensions."
+    assert (
+        test_set.data.size() == torch.empty(10000, 3, 32, 32).size()
+    ), "Expected a training set of 10000 instances with 32x32x3 dimensions."
