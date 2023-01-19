3f4fad8f4b14cc046bff2956b1be849e5314057c
Aimas Lund <aimas.lund@gmail.com>
2023-01-19 10:49:01 +0100

    Added google cloud function

--- cloud/functions/predict/main.py
+++ cloud/functions/predict/main.py
@@ -1,28 +1,33 @@
-from torch import device, argmax, Tensor
-from io import BytesIO
-from timm import create_model
-from http import HTTPStatus
-from PIL import Image
-from torchvision import transforms
 import logging
+import os
+from http import HTTPStatus
+from io import BytesIO
+
+from google.cloud import storage
+from PIL import Image
+from timm import create_model
+from torch import Tensor, argmax, device
+from torchvision import transforms
 
 log = logging.getLogger(__name__)
+bucket_name = "mlops-checkpoints"
 
-class PredictModel():
-    
-    def __init__(
-        self,
-        model_name: str,
-        num_classes: int,
-    ) -> None:
-        
+
+class PredictModel:
+    def __init__(self, model_name: str, num_classes: int, checkpoint_path: str) -> None:
+
         d = device("cpu")
         self.model_name = model_name
-        self.model = create_model(
-            model_name=model_name,
-            num_classes=num_classes,
-            pretrained=True,
-        ).to(d).eval()
+        self.model = (
+            create_model(
+                model_name=model_name,
+                num_classes=num_classes,
+                pretrained=False,
+                checkpoint_path=checkpoint_path,
+            )
+            .to(d)
+            .eval()
+        )
 
         self.class_mapping = {
             0: "airplane",
@@ -34,21 +39,18 @@ class PredictModel():
             6: "frog",
             7: "horse",
             8: "ship",
-            9: "truck"
+            9: "truck",
         }
-
 
     def predict(self, img: Tensor) -> int:
         predictions = self.model(img)
         return predictions
 
 
-def mlops_predict(request):
-    image_content = request.files.get('image')
+def mlops_predict_trained(request):
+    image_content = request.files.get("image")
 
-    transform = transforms.Compose([
-        transforms.ToTensor()
-    ])
+    transform = transforms.Compose([transforms.ToTensor()])
 
     with Image.open(BytesIO(image_content.read())) as img:
         input_tensor = transform(img)
@@ -56,9 +58,19 @@ def mlops_predict(request):
     log.info("Found checkpoints for the specified model.")
     log.info("Initializing model...")
 
+    client = storage.Client()
+    bucket = client.get_bucket(bucket_name)
+    file_name = "resnet10t-2023-01-18-22-18-01.pth"
+
+    os.makedirs("/tmp", exist_ok=True)
+
+    checkpoint_path = "/tmp/resnet10t-2023-01-18-22-18-01.pth"
+
+    blob = bucket.blob(file_name)
+    blob.download_to_filename(checkpoint_path)
+
     model = PredictModel(
-        model_name='resnet10t',
-        num_classes=10,
+        model_name="resnet10t", num_classes=10, checkpoint_path=checkpoint_path
     )
 
     log.info(f"Model created with parameters: {model.model.parameters()}")
@@ -67,10 +79,6 @@ def mlops_predict(request):
     pred_idx = argmax(prediction).item()
     predicted_class = model.class_mapping[pred_idx]
 
-    log.info(f"Predicted the class: \'{predicted_class}\'")
-    response = {
-        "prediction": predicted_class,
-        "message": HTTPStatus.OK.phrase,
-        "status-code": HTTPStatus.OK,
-    }
+    log.info(f"Predicted the class: '{predicted_class}'")
+
     return f"Predicted class: {predicted_class}"
