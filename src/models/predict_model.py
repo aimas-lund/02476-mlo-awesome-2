3aaa555df7e783ec73cf4211186bee1fa56c0263
antarlinam <antarlina.mukherjee@gmail.com>
2023-01-19 19:53:00 +0100

    added wandb table and add. arg in predict_model

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -5,10 +5,25 @@ import torch
 from src.models import _PATH_MODELS
 
 
-def validation(model, loss_func, dataloader, device):
+def validation(model, loss_func, dataloader, device,test_flag):
+    import wandb
+    from PIL import Image as im
+    class_mapping = {
+    0: "airplane",
+    1: "automobile",
+    2: "bird",
+    3: "cat",
+    4: "deer",
+    5: "dog",
+    6: "frog",
+    7: "horse",
+    8: "ship",
+    9: "truck"
+    }
     val_loss = 0.0
     val_correct = 0
     size_sampler = len(dataloader.sampler)
+    my_data = []
     with torch.no_grad():
         for images, labels in dataloader:
 
@@ -19,7 +34,25 @@ def validation(model, loss_func, dataloader, device):
             val_loss += loss.item() * images.size(0)
             _, pred = torch.max(y_hat.data, 1)
             val_correct += (pred == labels.long().squeeze()).sum().item()
-
+            if test_flag == 1:
+                for ii in range(len(images)):
+                    img2 = images[ii].reshape(32,32,3).numpy()
+                    img2 = img2.astype(np.uint8)
+                    img2 = im.fromarray(img2, mode="RGB")
+                    # print(img2)
+                    # if ii == 0:
+                    #     try:
+                    #         print(img2.shape)
+                    #     except:
+                    #         continue
+                    #     try:
+                    #         print(type(img2))
+                    #     except:
+                    #         continue
+                    subb = [wandb.Image(img2),class_mapping[pred.numpy()[ii]],class_mapping[labels.long().squeeze().numpy()[ii]]]
+                    my_data.append(subb)
+                test_table = wandb.Table(data=my_data, columns=["images","pred","actual"])
+                wandb.log({"table": test_table})
     return np.round(val_loss / size_sampler, 4), np.round(
         val_correct * 100.0 / size_sampler, 3
     )
